3b36ec70e1ecd69bfd83c089c170a864ad1dffe4
root <root@pyro-quantal64.(none)>
2014-03-31 04:23:55 +0000

    have bootstrap.sh prompt the user and create a password

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 sudo adduser --disabled-password --gecos "" zconnelly13
 sudo adduser zconnelly13 sudo
+sudo passwd zconnelly13
 echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
 wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
 sudo apt-get update
