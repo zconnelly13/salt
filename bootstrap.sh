81e61b65aef00cef4d135571b384d70cea64f9bd
root <root@pyro-quantal64.(none)>
2014-03-31 03:58:11 +0000

    fix issue with directory permissions and add a call to apt-get update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 sudo adduser --disabled-password --gecos "" zconnelly13
-sudo apt-get install git
-cd /srv
-sudo mkdir salt
-cd salt
-sudo git clone https://github.com/zconnelly13/salt .
+sudo adduser zconnelly13 sudo
+sudo chown -R zconnelly13 /home/zconnelly13
+sudo su zconnelly13
 echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
 wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install salt-minion
 sudo salt-call --local state.highstate
